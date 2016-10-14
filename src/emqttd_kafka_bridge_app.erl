d4f3244efe9456034f7f0ae528f50d6dc8a45407
Jerry Duan <jinrui.duan@icloud.com>
2016-10-13 22:07:04 -0400

    Debug

--- src/emqttd_kafka_bridge_app.erl
+++ src/emqttd_kafka_bridge_app.erl
@@ -8,8 +8,8 @@
 start(_StartType, _StartArgs) ->
     gen_conf:init(emqttd_kafka_bridge),
     {ok, Sup} = emqttd_kafka_bridge_sup:start_link(),
-    ok = emqttd_access_control:register_mod(auth, emqttd_auth_kafka_bridge, []),
-    ok = emqttd_access_control:register_mod(acl, emqttd_acl_kafka_bridge, []),
+    %% ok = emqttd_access_control:register_mod(auth, emqttd_auth_kafka_bridge, []),
+    %% ok = emqttd_access_control:register_mod(acl, emqttd_acl_kafka_bridge, []),
     emqttd_kafka_bridge:load(application:get_all_env()),
     {ok, Sup}.
 
