47c462c33c5910ab739ef477d54b56b3ad93a16e
root <jinrui.duan@icloud.com>
2016-10-19 17:31:53 +0000

    Debug

--- src/emq_kafka_bridge_app.erl
+++ src/emq_kafka_bridge_app.erl
@@ -6,7 +6,7 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    gen_conf:init(emq_kafka_bridge),
+    %%gen_conf:init(emq_kafka_bridge),
     {ok, Sup} = emq_kafka_bridge_sup:start_link(),
     %% ok = emqttd_access_control:register_mod(auth, emqttd_auth_kafka_bridge, []),
     %% ok = emqttd_access_control:register_mod(acl, emqttd_acl_kafka_bridge, []),
